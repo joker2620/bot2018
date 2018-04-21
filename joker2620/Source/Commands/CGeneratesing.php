7c7c4652c62f8e81b5c42365f5961c7e53fa9884
joker2620 <joker2000joker@list.ru>
2018-04-21 14:38:39 +0300

    add Sing-Art library.

--- joker2620/Source/Commands/CGeneratesing.php
+++ joker2620/Source/Commands/CGeneratesing.php
@@ -3,8 +3,9 @@ declare(strict_types = 1);
 
 namespace joker2620\Source\Commands;
 
+use joker2620\SingArt\SignArt;
 use joker2620\Source\Modules\CommandsTemplate;
-use joker2620\Source\Plugins\AutoSing\SignArt;
+use joker2620\Source\Setting\Config;
 
 
 /**
@@ -31,7 +32,20 @@ class CGeneratesing extends CommandsTemplate
      */
     public function runCommand(array $matches)
     {
-        $content  = new SignArt();
+        $content = new SignArt();
+        $content->setDirectory(Config::getConfig()['DIR_IMAGES'] . '/sign');
+        $image_base = [
+            ['0.png', 320, 247, 383, 709, 24],
+            ['1.png', 201, 111, 383, 190, 19],
+            ['2.png', 409, 177, 396, 508, -10],
+            ['3.png', 606, 310, 165, 653, 7],
+            ['4.png', 281, 121, 659, 342, 10],
+            ['5.png', 386, 202, 261, 562, -9],
+            ['6.png', 284, 188, 675, 507, 25],
+            ['7.png', 191, 90, 253, 128, -9],
+            ['8.png', 328, 75, 209, 325, -1]
+        ];
+        $content->addImageBase($image_base);
         $autosign = $content->generate($matches[1][0], [0, 34, 56]);
         $photo    = $this->vkapi->uploadPhoto($this->user->getId(), $autosign);
         $content->imageDestroy($autosign);
